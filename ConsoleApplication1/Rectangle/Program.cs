a870eb17a251e7529a6f7a876bc79301b0c5f9cb
HoangLong <nguyenphamhoanglong2000@gmail.com>
2019-10-30 11:33:27 +0700

    On-bài

--- ConsoleApplication1/Rectangle/Program.cs
+++ ConsoleApplication1/Rectangle/Program.cs
@@ -10,6 +10,10 @@ namespace Rectangle
     {
         static void Main(string[] args)
         {
+            Rectangle r1 = new Rectangle();
+            Console.WriteLine("The Rectangle has Length of " + r1.getLength() + " Width of " + r1.getWidth() + " and Perimeter of " + r1.getPerimeter() + " and Area of " + r1.getArea());
+            Rectangle r2 = new Rectangle(2,1);
+            Console.WriteLine("The Rectangle has Length of " + r2.getLength() + " Width of " + r2.getWidth() + " and Perimeter of " + r2.getPerimeter() + " and Area of " + r2.getArea());
         }
     }
 }
---
+++ ConsoleApplication1/Rectangle/Rectangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Rectangle
+    {
+        private float length;
+        private float width;
+
+        public Rectangle()
+        {
+            length = 1.0f;
+            width = 1.0f;
+        }
+        public Rectangle(float l , float w)
+        {
+            length = l;
+            width = w;
+        }
+        public float getLength()
+        {
+            return length;
+        }
+        public void setLength(float newL)
+        {
+            length = newL;
+        }
+        public float getWidth()
+        {
+            return width;
+        }
+        public void setWidth(float newW)
+        {
+            length = newW;
+        }
+        public double getArea()
+        {
+            return length * width;
+        }
+        public double getPerimeter()
+        {
+            return (length + width) * 2;
+        }
+        public override string ToString()
+        {
+            return String.Format("Rectangle[length{0} , width{1}]", length, width);
+            //return base.ToString();
+        }
+    }
+}

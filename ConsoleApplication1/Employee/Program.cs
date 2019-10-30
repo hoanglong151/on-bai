8647ee436551f3c3be58b8b69147857bb22e930a
HoangLong <nguyenphamhoanglong2000@gmail.com>
2019-10-30 11:50:31 +0700

    On-bai

---
+++ ConsoleApplication1/Employee/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            Console.WriteLine("The Rectangle has Length of " + r1.getLength() + " Width of " + r1.getWidth() + " and Perimeter of " + r1.getPerimeter() + " and Area of " + r1.getArea());
+            Rectangle r2 = new Rectangle(2,1);
+            Console.WriteLine("The Rectangle has Length of " + r2.getLength() + " Width of " + r2.getWidth() + " and Perimeter of " + r2.getPerimeter() + " and Area of " + r2.getArea());
+        }
+    }
+}

ac233c9d979c6e752008f7e602b24b5bf7b254a8
HoangLong <nguyenphamhoanglong2000@gmail.com>
2019-11-04 21:45:11 +0700

    Signed-off-by: HoangLong <nguyenphamhoanglong2000@gmail.com>

---
+++ ConsoleApplication1/MyPoint/MyCircle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPoint
+{
+    class MyCircle
+    {
+        private MyPoint center;
+        private int radius;
+
+        public MyCircle()
+        {
+            center.GetX();
+            center.GetY();
+            radius = 1;
+        }
+        public MyCircle(int x , int y , int radius)
+        {
+            this.radius = radius;
+            center = new MyPoint(x, y);
+        }
+        public MyCircle(MyPoint center , int radius)
+        {
+            this.center = center;
+            this.radius = radius;
+        }
+        public int getRadius()
+        {
+            return radius;
+        }
+        public void setRadius(int radius)
+        {
+            this.radius = radius;
+        }
+        public MyPoint getCenter()
+        {
+            return center;
+        }
+        public void setCenter(MyPoint center)
+        {
+            this.center = center;
+        }
+        public int getCenterX()
+        {
+            return center.GetX();
+        }
+        public void setCenterX(int x)
+        {
+            
+        }
+    }
+}
